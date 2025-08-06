64bbed7a39ef022ed416eac64b7f4c8a359393bc
ARSHIYA SHAIK <shaikarshiyaa17@gmail.com>
2025-08-06 05:48:39 +0530

    Update and rename

--- Stacks/1475. Final Prices With a Special Discount in a Shop.java
+++ Stacks/1475. Final Prices With a Special Discount in a Shop.java
@@ -1,4 +1,4 @@
-You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+/*You are given an integer array prices where prices[i] is the price of the ith item in a shop.
 
 There is a special discount for items in the shop. If you buy the ith item, then you will receive a discount equivalent to prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i]. Otherwise, you will not receive any discount at all.
 
@@ -32,7 +32,7 @@ Constraints:
 1 <= prices[i] <= 1000
 
 Source code:
-
+*/
 class Solution {
     public int[] finalPrices(int[] prices) {
         Stack<Integer> stack=new Stack<>();
@@ -47,4 +47,5 @@ class Solution {
         return prices;
 
     }
+
 }
