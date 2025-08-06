d06073894d826e71ba5a6e85706c230500e4d77e
ARSHIYA SHAIK <shaikarshiyaa17@gmail.com>
2025-08-06 05:51:34 +0530

    Update and rename

--- Stacks/445. Add Two Numbers II.java
+++ Stacks/445. Add Two Numbers II.java
@@ -1,4 +1,4 @@
-You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+/*You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -26,7 +26,7 @@ The number of nodes in each linked list is in the range [1, 100].
 It is guaranteed that the list represents a number that does not have leading zeros.
 
 Source code:
-
+*/
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -72,4 +72,5 @@ class Solution {
         }
         return dummy.next;
     }
+
 }
