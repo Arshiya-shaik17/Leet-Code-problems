df4641d630ec96874993b6bf5dabfb79b0b7dba4
ARSHIYA SHAIK <shaikarshiyaa17@gmail.com>
2025-08-09 21:45:44 +0530

    Add files via upload

---
+++ Sorting/912. Sort an Array.java
@@ -0,0 +1,67 @@
+/*
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessarily unique.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-5 * 104 <= nums[i] <= 5 * 104
+SOurce code:
+*/
+//Merge sort
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int[] mergeSort(int[] nums, int left, int right) {
+        if (left == right) {
+            return new int[]{nums[left]};
+        }
+
+        int mid = left + (right - left) / 2;
+        int[] leftArray = mergeSort(nums, left, mid);
+        int[] rightArray = mergeSort(nums, mid + 1, right);
+
+        return merge(leftArray, rightArray);
+    }
+
+    private int[] merge(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                merged[k++] = left[i++];
+            } else {
+                merged[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            merged[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            merged[k++] = right[j++];
+        }
+
+        return merged;
+    }
+}
